616ec834edc840dda66bdad2a34039d22e8085ef
Chanakyha <vetrichanakyha2003@gmail.com>
2022-08-06 03:04:09 +0530

    Added new Medium Story

--- assets/data/medium-post.mjs
+++ assets/data/medium-post.mjs
@@ -1,15 +1,21 @@
 const data = [
   {
-    title: "Steps to add Firebase to your React App",
-    imageUrl: "./assets/img/mediumThumnail/Medium-1.png",
+    title: "Google Authentication using Firebase",
+    imageUrl: "./assets/img/mediumThumnail/Medium-3.png",
     redirectUrl:
-      "https://medium.com/@vetrichanakyha2003/how-to-add-firebase-to-your-react-app-9b69be2766c3",
+      "https://medium.com/@vetrichanakyha2003/google-authentication-using-firebase-1e28b728cb6a",
   },
   {
     title: "Manage Firebase Authentication using Custom Hooks",
     imageUrl: "./assets/img/mediumThumnail/Medium-2.png",
     redirectUrl:
       "https://medium.com/@vetrichanakyha2003/create-custom-hooks-to-manage-user-authentication-in-a-react-firebase-app-c0c9abcf8b05",
+  },
+  {
+    title: "Steps to add Firebase to your React App",
+    imageUrl: "./assets/img/mediumThumnail/Medium-1.png",
+    redirectUrl:
+      "https://medium.com/@vetrichanakyha2003/how-to-add-firebase-to-your-react-app-9b69be2766c3",
   },
 ];
 
