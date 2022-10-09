a1563ef7f2efdc7081f073f9dd424d7b08688166
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-09 19:54:39 +0530

    added post route for blogs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const sanityClient = require("@sanity/client");
+const imageUrlBuilder = require("@sanity/image-url");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,3 +10,29 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.use(express.static("public"));
 app.use(express.json());
+
+const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2021-10-21",
+  useCdn: false,
+});
+
+const builder = imageUrlBuilder(client);
+
+function sanityImgURL(source) {
+  return builder.image(source);
+}
+
+app.post("/blogs", async (req, res) => {
+  const groq = `*[_type=="blogs"] {title, thumbnail,redirect_url}`;
+
+  const data = await client.fetch(groq);
+
+  res.status(200).json(
+    data.map((blog) => ({
+      ...blog,
+      thumbnail: sanityImgURL(blog.thumbnail).url(),
+    }))
+  );
+});
