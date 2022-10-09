d2fdcd13e3585c2b624d561d5383f9684ed2c64b
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-09 20:02:01 +0530

    pulling out blogs from sanity

--- public/assets/js/mediumPost.mjs
+++ public/assets/js/mediumPost.mjs
@@ -1,18 +1,17 @@
-import mediumPosts from "../data/medium-post.mjs";
+const mediumPosts = await (await fetch("/blogs", { method: "POST" })).json();
 
 const docRef = document.querySelector("#medium-post-container");
 
-window.onload = () =>
-  mediumPosts?.map(({ redirectUrl, imageUrl, title }) => {
-    docRef.innerHTML += `
+mediumPosts?.map(({ redirect_url, thumbnail, title }) => {
+  docRef.innerHTML += `
     <a target="_blank"
-    href="${redirectUrl}"
+    href="${redirect_url}"
     class="card blogCard">
 
-    <img src="${imageUrl}" class="card-img-top" alt="...">
+    <img src="${thumbnail}" class="card-img-top" alt="...">
     <div class="card-body" style="padding:.4rem">
       <p class="card-text">${title}</p>
     </div>
   </a>
     `;
-  });
+});
