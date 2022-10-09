09a2995dd0558bf26a63b0c697727c65e0c7ef38
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-09 16:09:23 +0530

    Added Server Script and Installed Express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.use(express.static("public"));
+app.use(express.json());
