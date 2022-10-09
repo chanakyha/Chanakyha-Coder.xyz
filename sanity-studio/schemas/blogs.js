2a03317d8750fb2dba345d508a4ef49d14eef61f
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-09 16:27:25 +0530

    Added Blog Schemas

---
+++ sanity-studio/schemas/blogs.js
@@ -0,0 +1,33 @@
+export default {
+  name: "blogs",
+  title: "Blogs",
+  type: "document",
+  fields: [
+    {
+      name: "id",
+      title: "ID",
+      type: "number",
+    },
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
+    },
+    {
+      name: "redirect_url",
+      title: "Redirect URL",
+      type: "string",
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
